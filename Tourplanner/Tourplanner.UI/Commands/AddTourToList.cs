d67274a9645d2bc62f5518e77e6aad8d87c925c1
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2025-08-27 14:33:35 +0200

    AddTourTo list boilerplate

---
+++ Tourplanner/Tourplanner.UI/Commands/AddTourToList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Tourplanner.UI.ViewModels;
+
+namespace Tourplanner.UI.Commands
+{
+    internal class AddTourToList : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private TourListViewModel viewModel;
+
+        public AddTourToList(TourListViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        // required in interface, not used
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            viewModel.AddClicked();
+            Trace.WriteLine("Button clicked - Add Tour");
+        }
+    }
+}
