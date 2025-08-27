c1112fa37b097a648f528b81e09cb07aa16368fc
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2025-08-27 14:35:23 +0200

    ViewModels

---
+++ Tourplanner/Tourplanner.UI/ViewModels/TourLogsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourplanner.UI.ViewModels
+{
+    internal class TourLogsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
