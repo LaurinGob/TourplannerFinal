ab72c54a0b8bc4d33e43e8953daedf73c1048712
LaurinGob <113621416+LaurinGob@users.noreply.github.com>
2025-08-27 14:40:03 +0200

    Create RemoveLogFromList.cs

---
+++ Tourplanner/Tourplanner.UI/Commands/RemoveLogFromList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+using Tourplanner.UI.ViewModels;
+
+namespace TourPlanner.UI.Commands
+{
+    internal class RemoveLogFromList : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private TourLogsViewModel viewModel;
+
+        public RemoveLogFromList(TourLogsViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+
+            viewModel.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(viewModel.SelectedIndex))
+                {
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return viewModel.SelectedIndex != -1;
+        }
+
+        public void Execute(object? parameter)
+        {
+            viewModel.RemoveClicked();
+        }
+    }
+}
